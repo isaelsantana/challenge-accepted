f6585a7db0b44287e30712ece4c1bb2808ce8772
Isael <isaelsantana.silva@gmail.com>
2017-11-18 17:10:53 -0200

    Correção app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('./config/server');
 
+var port = process.env.PORT || 3001;
 
-app.listen(3001, function(){
-    console.log("Servidor ligado porta 3001");
+app.listen(port, function(){
+    console.log("Servidor ligado porta" + port);
 });
